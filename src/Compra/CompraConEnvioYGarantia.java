c70b0c579435371564f56d3be1915411b8ce654b
ClaudioCristianLopez <fulanitoc78@gmail.com>
2020-08-25 16:14:37 -0300

    Creamos clase Descuento y Cupon mas pruebas

--- src/Compra/CompraConEnvioYGarantia.java
+++ src/Compra/CompraConEnvioYGarantia.java
@@ -1,9 +1,12 @@
 package Compra;
 
+import javax.swing.*;
+
 public class CompraConEnvioYGarantia extends Compra {
 
     private static final float COSTOGARANTIA = (float) 1.1;
     private static final int COSTOENVIO = 100;
+    private static final int VALORMINIMOPARABONIFICARENVIO = 5000;
 
 
 //-------------------------------------------------------------------
@@ -20,7 +23,13 @@ public class CompraConEnvioYGarantia extends Compra {
 //-------------------------------------------------------------------
     @Override
     public int cobrarExtras(int precioTotalDeLosProductos) {
-        return (int) (COSTOENVIO+precioTotalDeLosProductos*COSTOGARANTIA);
+        System.out.println((6000 * 1.1)+0);
+        float monto = (float) 0.0;
+        if (precioTotalDeLosProductos > VALORMINIMOPARABONIFICARENVIO)
+            monto = (int) (precioTotalDeLosProductos*COSTOGARANTIA);
+        else
+            monto = (int) (COSTOENVIO+precioTotalDeLosProductos*COSTOGARANTIA);
+        return (int) monto;
     }
 //-------------------------------------------------------------------
 
